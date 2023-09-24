3928fa87d679eef9814d18971d8deed66e9536c4
edubew <edubew98@gmail.com>
2023-09-24 16:47:10 +0300

    Install devise, setting up different controllers for admin and client side

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    registrations: 'admin/registrations'
+  }
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  root 'pages#home'
+  get 'pages/users'
+  get 'pages/admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
